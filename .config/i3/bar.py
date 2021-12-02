5352271843b3d3988eb416ff61d36d791574776d
LeadSeason <pauli.tiittanen113@gmail.com>
2021-12-02 11:22:11 +0200

    idk

--- .config/i3/bar.py
+++ .config/i3/bar.py
@@ -1,7 +1,26 @@
-from i3pystatus import Status
+from i3pystatus import Status, IntervalModule
+import i3pystatus
+import os
+import json
 
-status = Status()
+from i3pystatus.core.modules import IntervalModule
 
+
+class cputemp(IntervalModule):
+    format = "{temp} °C"
+    def init(self):
+        pass
+
+    def run(self):
+        json_data = json.loads(str(os.popen("sensors -j").read()))
+        tmp = json_data["nct6797-isa-0a20"]["AUXTIN0"]["temp3_input"]
+        self.output = {"full_text": f"{str(tmp)}°C", "color": "#FFFFFF"}
+
+
+
+status = Status(logfile='$HOME/.cache/i3pystatus.log')
+
+status.register("text", text="")
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
@@ -11,10 +30,18 @@ status.register("clock",
 # Shows the average load of the last minute and the last 5 minutes
 # (the default value for format is used)
 status.register("load")
+status.register("cpu_usage")
 
+status.register(cputemp())
+
+status.register("mem",
+    format="mem:{used_mem}:{percent_used_mem}%")
+
+status.register("amdgpu",
+    format="gpu:{gpu_usage}% {temp}°C")
 # Shows your CPU temperature, if you have a Intel CPU
-status.register("temp",
-    format="{temp:.0f}°C",)
+# status.register("temp",
+#     format="{temp:.0f}°C",)
 
 # The battery monitor has many formatting options, see README for details
 
@@ -27,27 +54,27 @@ status.register("temp",
 # goes below 5 percent while discharging. The block will also color RED.
 # If you don't have a desktop notification demon yet, take a look at dunst:
 #   http://www.knopwob.org/dunst/
-status.register("battery",
-    format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
-    alert=True,
-    alert_percentage=5,
-    status={
-        "DIS": "↓",
-        "CHR": "↑",
-        "FULL": "=",
-    },)
-
-# This would look like this:
-# Discharging 6h:51m
-status.register("battery",
-    format="{status} {remaining:%E%hh:%Mm}",
-    alert=True,
-    alert_percentage=5,
-    status={
-        "DIS":  "Discharging",
-        "CHR":  "Charging",
-        "FULL": "Bat full",
-    },)
+# status.register("battery",
+#     format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+#     alert=True,
+#     alert_percentage=5,
+#     status={
+#         "DIS": "↓",
+#         "CHR": "↑",
+#         "FULL": "=",
+#     },)
+# 
+# # This would look like this:
+# # Discharging 6h:51m
+# status.register("battery",
+#     format="{status} {remaining:%E%hh:%Mm}",
+#     alert=True,
+#     alert_percentage=5,
+#     status={
+#         "DIS":  "Discharging",
+#         "CHR":  "Charging",
+#         "FULL": "Bat full",
+#     },)
 
 # Displays whether a DHCP client is running
 # status.register("runwatch",
@@ -62,13 +89,13 @@ status.register("battery",
 #
 # Note: the network module requires PyPI package netifaces
 status.register("network",
-    interface="enp3s0",
+    interface="enp39s0",
     format_up="{v4cidr}",)
 
 # Note: requires both netifaces and basiciw (for essid and quality)
-status.register("network",
-    interface="wlp4s0",
-    format_up="{essid} {quality:03.0f}%",)
+#status.register("network",
+#    interface="wlp4s0",
+#    format_up="{essid} {quality:03.0f}%",)
 
 # Shows disk usage of /
 # Format:
