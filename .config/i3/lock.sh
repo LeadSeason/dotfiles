c111b59e32ff509ebad734d3bd54d5c53d5fbe58
LeadSeason <pauli.tiittanen113@gmail.com>
2021-12-08 17:55:38 +0200

    git commit -m "idk"; git push

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -4,14 +4,5 @@ if pgrep -x "i3lock" >/dev/null
 then
     printf "already locked aborting ..."
 else
-    # Take a screenshot
-    scrot /tmp/screen_locked.png
-
-    # Pixellate it 10x
-    mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-
-    # Lock screen displaying this image.
-    i3lock -i /tmp/screen_locked.png
-
-    rm -f /tmp/screen_locked.png
+    i3lock --blur 5 --indicator --clock
 fi
