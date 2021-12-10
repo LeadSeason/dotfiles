689135cc460297700774cfc375ec3c8372c2d10c
LeadSeason <pauli.tiittanen113@gmail.com>
2021-12-10 13:03:03 +0200

    video screen recorder

---
+++ .config/i3/video.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+rm -f ~/.cache/vidcap.mp4 ~/.cache/vidcap.ogv
+
+read -r X Y W H < <(slop -l -c 0.3,0.4,0.6,0.4 -f '%x %y %w %h');
+
+recordmydesktop -x=$X -y=$Y --width=$W --height=$H --no-sound -o ~/.cache/vidcap.ogv;
+notify-send -i none Recorder "Screen recorder has stoped"
+ffmpeg -i ~/.cache/vidcap.ogv -vcodec libx264 ~/.cache/vidcap.mp4;
+python3 ~/Pictures/FlameShot-Custom-Uploader/main.py ~/.cache/vidcap.mp4;
+
+rm -f ~/.cache/vidcap.mp4 ~/.cache/vidcap.ogv
