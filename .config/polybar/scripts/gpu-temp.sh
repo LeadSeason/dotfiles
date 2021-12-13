4e499ff724c33244eeed5cee0f7a96aded377a1a
LeadSeason <pauli.tiittanen113@gmail.com>
2021-12-13 12:07:24 +0200

    clean up 2 the long awaited sequel

--- .config/polybar/scripts/gpu-temp.sh
+++ .config/polybar/scripts/gpu-temp.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-var=$(sensors -u)
+var=$(sensors -u >2 /dev/null)
 
 # check if radeon
 if [[ $var == *"amdgpu"* ]]
 then
-    >&2 echo amdgpu
     var=${var##*amdgpu}
     var=${var##*edge}
     var=${var##*temp1_input}
@@ -14,14 +13,13 @@ then
 
 elif [[ $var == *"radeon"* ]]
 then
-    >&2 echo radeon
     var=${var##*radeon}
     var=${var##*temp1_input}
     var=${var:2}
     var=${var%% *}
 
 else
-    >&2 echo "This script hasnt picked up your gpu\nThis script should work only for radeon and amdgpu's"
+    >&2 echo "This script hasnt picked up your gpu\nor your gpu inst in sensors try detect-sensors to fix\nThis script should work only for radeon and amdgpu's"
 fi
 
 printf "%.0f\n" ${var}
