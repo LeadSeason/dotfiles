65336915e8744f6dc4ea23909e584ae6a9c781a3
LeadSeason <pauli.tiittanen113@gmail.com>
2021-12-13 11:55:06 +0200

    test 1 gpu temp radeon support

--- .config/polybar/scripts/gpu-temp.sh
+++ .config/polybar/scripts/gpu-temp.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
 var=$(sensors -u)
-var=${var##*edge}
-var=${var##*temp1_input}
-var=${var:2}
-var=${var%% *}
+if [[ $var == *"amdgpu"* ]];
+then
+    >&2 echo amdgpu
+    var=${var##*amdgpu}
+    var=${var##*edge}
+    var=${var##*temp1_input}
+    var=${var:2}
+    var=${var%% *}
+fi
+
+if [[ $var == *"radeon"* ]];
+then
+    >&2 echo radeon
+    var=${var##*radeon}
+    var=${var##*temp1_input}
+    var=${var:2}
+    var=${var%% *}
+fi
 printf "%.0f\n" ${var}
