04c4eb8de999dda38ec397810eddae6a222c8108
LeadSeason <pauli.tiittanen113@gmail.com>
2022-12-07 07:54:49 +0200

    Fixed showing one update after update

--- .config/waybar/scipts/getupdates-update.sh
+++ .config/waybar/scipts/getupdates-update.sh
@@ -2,7 +2,7 @@
 
 yay -Syu --noconfirm --sudoloop
 sudo pacman -Rcns $(pacman -Qdtq) --noconfirm
-echo 0 > /tmp/updates
+echo -n > /tmp/updates
 
 printf "\e[1;32m ____   ___  _   _ _____
 |  _ \ / _ \| \ | | ____|
