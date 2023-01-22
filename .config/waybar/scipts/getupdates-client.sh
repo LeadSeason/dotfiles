aa30f945be122f73959b878fc7386e48aa41b7ac
LeadSeason <pauli.tiittanen113@gmail.com>
2023-01-22 23:23:36 +0200

    Made tooltip have updated pkg's

--- .config/waybar/scipts/getupdates-client.sh
+++ .config/waybar/scipts/getupdates-client.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if ! updates=$(cat /tmp/updates 2> /dev/null | wc -l); then
-    updates=0
+if ! updateslen=$(cat /tmp/updates 2> /dev/null | wc -l); then
+    updateslen=0
 fi
 
-echo "$updates"
+if ! updates=$(cat /tmp/updates 2> /dev/null | sed -rz 's/\n/\\n/g'); then
+	updates=\"File /tmp/updates not found\"
+fi
+
+echo "{\"text\": \"$updateslen\", \"alt\": \"none\", \"tooltip\": \"${updates::-2}\", \"class\": \"none\"}" | jq --unbuffered --compact-output
