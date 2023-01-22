a4ffbe84e362c662b81e8731a9d52c54dab36769
LeadSeason <pauli.tiittanen113@gmail.com>
2023-01-22 23:37:29 +0200

    Fix stuff

--- .config/waybar/scipts/getupdates-client.sh
+++ .config/waybar/scipts/getupdates-client.sh
@@ -8,4 +8,8 @@ if ! updates=$(cat /tmp/updates 2> /dev/null | sed -rz 's/\n/\\n/g'); then
 	updates=\"File /tmp/updates not found\"
 fi
 
-echo "{\"text\": \"$updateslen\", \"alt\": \"none\", \"tooltip\": \"${updates::-2}\", \"class\": \"none\"}" | jq --unbuffered --compact-output
+if [ $updateslen -gt 2 ]; then
+	updates=${updates::-2}
+fi
+
+echo "{\"text\": \"$updateslen\", \"alt\": \"none\", \"tooltip\": \"$updates\", \"class\": \"none\"}" | jq --unbuffered --compact-output
