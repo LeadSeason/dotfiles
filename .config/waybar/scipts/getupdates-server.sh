1b9c5967605ec1c67ed75d8870c5914118117c3c
LeadSeason <pauli.tiittanen113@gmail.com>
2023-01-22 13:10:19 +0200

    Made integration with systemd smoother

--- .config/waybar/scipts/getupdates-server.sh
+++ .config/waybar/scipts/getupdates-server.sh
@@ -6,12 +6,4 @@ do
 	yay -Qau 2> /dev/null >> /tmp/updates
 
 	echo "Avalable packages to update $(cat /tmp/updates 2> /dev/null | wc -l)"
-
-	for i in {1..600}
-	do
-		sleep 1
-		if ! pgrep -x waybar > /dev/null; then
-			exit 0
-		fi
-	done
 done
