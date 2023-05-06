c404d2f90382069776f44abaa832d6876ba0d27c
LeadSeason <pauli.tiittanen113@gmail.com>
2023-05-06 23:29:02 +0300

    new monitor setup (bit late)

--- .config/i3/config.sh
+++ .config/i3/config.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-xrandr --output VGA-1-1 --off
-xrandr --output DisplayPort-0 --rate 144 --mode 1920x1080
-xrandr --output HDMI-A-0 --auto --left-of DisplayPort-0
-xrandr --output DVI-I-1-1 --rate 144 --mode 1920x1080 --right-of DisplayPort-0
+xrandr --output DisplayPort-0 --auto
+xrandr --output DisplayPort-1 --auto --left-of DisplayPort-0
+xrandr --output DisplayPort-2 --auto --right-of DisplayPort-0
 
 killall -q xsettingsd &
 killall -q picom &
