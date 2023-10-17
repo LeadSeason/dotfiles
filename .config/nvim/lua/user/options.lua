8dd6d86fb02de27a0cf1a177334e5dcbe31604f2
LeadSeason <pauli.tiittanen113@gmail.com>
2023-10-17 19:25:31 +0300

    I have had enough of nvim, astronvimrofi -show🤘

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,30 +1,30 @@
--- vim.opt.autochdir = true
--- vim.opt.expandtab = true
-vim.g.neovide_cursor_vfx_mode = "wireframe"
-vim.g.neovide_refresh_rate = 165
-vim.opt.colorcolumn = "80"
-vim.opt.autoindent = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-vim.opt.cursorline = true
-vim.opt.fileencoding = "utf-8"
-vim.opt.guifont = "Iosevka"
-vim.opt.modeline = true
-vim.opt.mouse = "a"
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 8
-vim.opt.shiftwidth = 4
-vim.opt.showmode = false
-vim.opt.showtabline = 0
-vim.opt.sidescrolloff = 8
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.swapfile = false
-vim.opt.tabstop = 4
-vim.opt.termguicolors = true
-vim.opt.timeoutlen = 300
-vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir/"
-vim.opt.undofile = true
-vim.opt.updatetime = 300
-vim.opt.wrap = false
+-- set vim options here (vim.<first_key>.<second_key> = value)
+return {
+  opt = {
+    -- set to true or false etc.
+    relativenumber = true, -- sets vim.opt.relativenumber
+    number = true, -- sets vim.opt.number
+    spell = false, -- sets vim.opt.spell
+    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+    wrap = false, -- sets vim.opt.wrap
+  },
+  g = {
+    mapleader = " ", -- sets vim.g.mapleader
+    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true, -- enable completion at start
+    autopairs_enabled = true, -- enable autopairs at start
+    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+  },
+}
+-- If you need more control, you can use the function()...end notation
+-- return function(local_vim)
+--   local_vim.opt.relativenumber = true
+--   local_vim.g.mapleader = " "
+--   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
+--   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
+--
+--   return local_vim
+-- end
