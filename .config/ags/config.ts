e0ba74b1f849be3ee448e95b154b774138a53742
LeadSeason <admin@leadseason.eu>
2025-10-26 21:22:05 +0200

    Meaw :3

--- .config/ags/config.ts
+++ .config/ags/config.ts
@@ -4,9 +4,14 @@ export default class Config {
     static instanceName = "wam";
 
     static instanceSrcFolder = `${GLib.get_user_config_dir()}/ags`
-    static osIcon = `${this.instanceSrcFolder}/themes/icons/panarch.png`
+    static osIcon = `${this.instanceSrcFolder}/themes/icons/polyarch.png`
 
     static instanceCacheDir= `${GLib.get_user_cache_dir()}/${this.instanceName}`
     static cacheFile = `${this.instanceCacheDir}/cache.json`
     static cssPath = `${this.instanceCacheDir}/style.css`
 }
+
+
+
+
+
